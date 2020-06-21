5431e15e1c80dedca012624953d778a9302866e4
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-06-21 11:40:07 +0700

    Unload skrip-crud.js on L11

--- app/tampilan/templat/footer.php
+++ app/tampilan/templat/footer.php
@@ -9,6 +9,5 @@
 <script src="<?php echo BASIS_URL; ?>/aset/js/bootstrap.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="<?php echo BASIS_URL; ?>/aset/js/bootstrap.min.js"></script>
-<script src="<?php echo BASIS_URL; ?>/aset/js/skrip-crud.js"></script>
 </body>
 </html>
