4f3f61eddbe538cb9f3f0b2c2623d0fb88191763
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-06-23 18:26:10 +0700

    Simplifikasi

--- app/tampilan/templat/header.php
+++ app/tampilan/templat/header.php
@@ -2,10 +2,11 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $data['judul']; ?></title>
+	<title><?php echo $data['judul']; ?> - ALIMS</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo BASIS_URL; ?>/aset/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASIS_URL; ?>/aset/css/bootstrap.css">
 	<script src="<?php echo BASIS_URL; ?>/aset/js/jquery-3.4.1.slim.min.js"></script>
 	<script src="<?php echo BASIS_URL; ?>/aset/js/jquery-3.4.1.min.js"></script>
+	<script src="<?php echo BASIS_URL; ?>/aset/js/jquery.autocomplete.js"></script>
 </head>
 <body>
