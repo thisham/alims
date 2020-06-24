37524e51cb24910ec1d85107c795fdcad533e514
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-06-24 13:17:43 +0700

    Add Features

--- app/kontroler/gunakan.php
+++ app/kontroler/gunakan.php
@@ -100,6 +100,57 @@ class gunakan extends Kontroler
 		}
 	}
 
+	function adl($menu = '', $id = '', $aksi = '')
+	{
+		switch ($menu) {
+			case 'tambahin':
+				if ( $this->model('m_gunakan')->gnadl_tambah($id) > 0 ) {
+					Flasher::setFlash('Data penggunaan alat dalam laboratorium', 'telah dicatat', '', 'success');
+					header('location:' . BASIS_URL . '/gunakan/adl');
+					exit;
+				} else {
+					Flasher::setFlash('Data penggunaan alat dalam laboratorium', 'telah dicatat', '', 'danger');
+					header('location:' . BASIS_URL . '/gunakan/adl');
+					exit;
+				}
+				break;
+
+			case 'update':
+				switch ($aksi) {
+					case 'mulai':
+						$this->model('m_gunakan')->gnlab_mulai($id);
+						header('location:' . BASIS_URL . '/gunakan/adl/detail/' . $id);
+						break;
+					
+					case 'selesai':
+						$this->model('m_gunakan')->gnlab_selesai($id);
+						header('location:' . BASIS_URL . '/gunakan/adl/detail/' . $id);
+						break;
+
+					default:
+						header('location:' . BASIS_URL . '/gunakan/adl/detail/' . $id);
+						break;
+				}
+				break;
+			
+			default:
+				$data = array(
+					'judul'	=> 'Penggunaan Alat dalam Laboratorium',
+					'pages'	=> 'Penggunaan',
+					'newID'	=> $this->model('m_gunakan')->gnadl_idbaru(),
+					'mtkul'	=> $this->model('m_akademik')->mtk_list('aktif', 'list'),
+					'dosen'	=> $this->model('m_warga')->dsn_list(),
+					'gnadl'	=> $this->model('m_gunakan')->gnadl_list(),
+					'labs'	=> $this->model('m_inventaris')->lab_list()
+				);
+				$this->tampilkan('templat/header', $data);
+				$this->tampilkan('templat/navbar_dash', $data);
+				$this->tampilkan('gunakan/lab/index', $data);
+				$this->tampilkan('templat/footer', $data);
+				break;
+		}
+	}
+
 	// function ajax()
 	// {
 	// 	var_dump($_REQUEST);
