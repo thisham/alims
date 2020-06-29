84706e4d02895ca3fd753f36ba64d9ea6f9e254b
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-06-30 06:04:03 +0700

    Tambah Peta Akses

--- app/tampilan/beranda/index.php
+++ app/tampilan/beranda/index.php
@@ -1,10 +1,137 @@
 <div class="container">
 	<div class="card">
-		<div class="card-header">
-			<h4 class="card-title">wkwkwk</h4>
+		<div class="card-header text-center">
+			<h4 class="card-title"><b>PETA AKSES APLIKASI</b></h4>
+			<small>Update: Sidoarjo, 30 Juni 2020, 00.12 UTC+07</small>
 		</div>
 		<div class="card-body">
-			wkwkwk
+			<div class="row">
+				<div class="col mt-2">
+					<div class="card border-primary">
+						<div class="card-body">
+							<h5 class="card-title"><b>Penggunaan</b></h5>
+							<hr>
+							<ul>
+								<li>Laboratorium
+									<ul>
+										<li>Data Pemakaian</li>
+										<li>Tambah Data Pemakaian</li>
+									</ul>
+								</li>
+								<li>Alat dalam Laboratorium
+									<ul>
+										<li>Data Pemakaian</li>
+										<li>Tambah Data Pemakaian</li>
+									</ul>
+								</li>
+								<li>Alat Pinjam-Pakai
+									<ul>
+										<li>Data Pemakaian</li>
+										<li>Alat yang Dipinjam</li>
+										<li>Alat Rusak</li>
+										<li>Pengembalian</li>
+										<li>Tambah Data Pemakaian</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col mt-2">
+					<div class="card border-primary">
+						<div class="card-body">
+							<h5 class="card-title"><b>Data Inventaris</b></h5>
+							<hr>
+							<ul>
+								<li>Laboratorium
+									<ul>
+										<li>Data Laboratorium
+											<ul>
+												<li>Detail Per Laboratorium</li>
+												<li>Riwayat Pemakaian Laboratorium</li>
+												<li>Alat dalam Laboratorium</li>
+											</ul>
+										</li>
+										<li>Tambah Data Laboratorium</li>
+									</ul>
+								</li>
+								<li>Alat dalam Laboratorium
+									<ul>
+										<li>Data Alat
+											<ul>
+												<li>Detail Alat</li>
+												<li>Riwayat Pemakaian Alat</li>
+											</ul>
+										</li>
+										<li>Tambah Data Alat</li>
+									</ul>
+								</li>
+								<li>Alat Pinjam-Pakai
+									<ul>
+										<li>Data Alat Pinjam-Pakai</li>
+										<li>Tambah Data Alat Pinjam-Pakai</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>	
+				</div>
+			</div>
+			<div class="row mt-2">
+				<div class="col mt-2">
+					<div class="card border-primary">
+						<div class="card-body">
+							<h5 class="card-title"><b>Data Warga dan Akademik</b></h5>
+							<hr>
+							<ul>
+								<li>Mahasiswa
+									<ul>
+										<li>Data Mahasiswa
+											<ul>
+												<li>Detail Per Mahasiswa</li>
+												<li>Data Pemakaian Laboratorium dan Alat</li>
+											</ul>
+										</li>
+										<li>Tambah Data Mahasiswa</li>
+									</ul>
+								</li>
+								<li>Dosen
+									<ul>
+										<li>Data Dosen</li>
+										<li>Tambah Data Dosen</li>
+									</ul>
+								</li>
+								<li>Mata Kuliah
+									<ul>
+										<li>Data Mata Kuliah
+											<ul>
+												<li>Detail Mata Kuliah</li>
+											</ul>
+										</li>
+										<li>Tambah Data Mata Kuliah</li>
+									</ul>
+								</li>
+								<li>Program Studi
+									<ul>
+										<li>Data Program Studi</li>
+										<li>Tambah Program Studi</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col mt-2">
+					<div class="card border-primary">
+						<div class="card-body">
+							<h5 class="card-title"><b>삼파이 디 시니 앞뎉냐</b></h5>
+							<hr>
+							<h6 class="card-title"><b>바탓 앜힐 리릿 프록람:</b></h6>
+							<h2 class="card-title"><b>라부, 01 주리 2020</b></h2>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
