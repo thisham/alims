01723e1dc30d1851c32cd67cf610ef684d86cd7e
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-07-05 09:22:57 +0700

    Add some features

--- app/tampilan/dasbor/dasbor.php
+++ app/tampilan/dasbor/dasbor.php
@@ -1,11 +1,21 @@
 <div class="container mt-4">
 	<div class="container mt-4">
 		<h3>Selamat datang, <?php echo $_SESSION['nama']; ?>!</h3>
-		<p class="text-muted"><i>Anda masuk sebagai: <?php echo $_SESSION['hak_akses']; ?>, dengan akun <?php echo $_SESSION['user']; ?>.</i></p>
+		<p class="text-muted"><i>Anda masuk sebagai: <?php echo $_SESSION['hak_akses']; ?>, dengan akun <?php echo $_SESSION['user']; ?> sejak <?php echo $_SESSION['waktu_sesi']; ?>.</i></p>
 		<hr>
 	</div>
 	<div class="row">
-		<div class="col"><?php var_dump($_SESSION); ?></div>
+		<div class="col">
+			<div class="card">
+				<div class="card-body">
+					<div class="form-group"><b>Email:</b><br><?php echo $data['usr']['dtl']['email']; ?></div>
+					<div class="form-group"><b>No. HP:</b><br><?php echo $data['usr']['dtl']['hp']; ?></div>
+					<?php if ($data['usr']['dtl']['bio'] != ''): ?>
+						<div class="form-group"><b>Tentang Saya:</b><br><?php echo $data['usr']['dtl']['bio']; ?></div>
+					<?php endif ?>
+				</div>
+			</div>
+		</div>
 		<div class="col">
 			<div class="row">
 				<div class="col">
@@ -35,7 +45,7 @@
 					<div class="alert alert-primary">
 						<h6 class="alert-heading">Kontribusi</h6>
 						<hr>
-						<h3 class="alert-text"><?php echo $data['usr']['gpp'] + $data['usr']['gdl'] + $data['usr']['glb']; ?></h3>
+						<h3 class="alert-text"><?php echo $data['usr']['ctr']['gpp'] + $data['usr']['ctr']['gdl'] + $data['usr']['ctr']['glb']; ?></h3>
 					</div>
 				</div>
 			</div>
@@ -73,7 +83,4 @@
 			</div>
 		</div>
 	</div>
-	
-			
-	
 </div>
