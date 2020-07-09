028ef8a56a5b2dda638fecd7bd6f9617ba1eb150
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-07-09 19:08:30 +0700

    Update Halaman Tentang

--- app/tampilan/tentang/halaman.php
+++ app/tampilan/tentang/halaman.php
@@ -1,13 +1,18 @@
 <div class="container mt-4">
 	<div class="card">
 		<div class="card-body">
-			<div class="text-center">
-				<h1 class="card-title">Sertifikat Atribusi</h1>
+			<center>
+				<img src="<?php echo BASIS_URL; ?>/aset/img/logo.png">
+				<h3 class="card-title mt-4">
+					<b>Academic Laboratory Information and Management System</b>
+				</h3>
+				<p class="text-muted">Versi 1.3.2 - <i>Agathis Dammara</i></p>
 				<hr>
-				<div class="container">
-					<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Lisensi Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Academic Laboratory Information and Management System (ALIMS)</span> oleh <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/thisham" property="cc:attributionName" rel="cc:attributionURL">Hamdan Yuwafi Mastu Wijaya</a> disebarluaskan di bawah <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Lisensi Creative Commons Atribusi-BerbagiSerupa 4.0 Internasional</a>.
-				</div>
-			</div>
+				<p>Aplikasi berbasis web ini merupakan aplikasi yang dapat digunakan untuk memanajemen informasi di dalam laboratorium akademik. Aplikasi ini bersifat <i>open-source</i> sehingga siapapun berhak mengembangkannya dan menyesuaikan dengan keadaan di lingkungan kerjanya.</p>
+				<p>Aplikasi ini dibangun menggunakan <i>framework</i> <a href="https://github.com/thisham/framework-php-kmt-1.0">PHP-KMT</a> sebagai back-end dan Bootstrap 4.1 sebagai front-end.</p>
+				<br class="mt-2">
+				<p>Sebagai penghargaan kepada programmer, <b>mohon jangan menghapus teks footer</b>.</p>
+			</center>
 		</div>
 	</div>
 </div>
